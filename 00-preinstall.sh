6e94b01085b8f083124ec3941fa64499b07fd26a
Carlos Estay <cestay@redhat.com>
2025-05-14 09:23:01 -0400

    Update 00-preinstall.sh

--- 00-preinstall.sh
+++ 00-preinstall.sh
@@ -1,6 +1,4 @@
-read -p "Enter cluster wildcard [apps.cluster-xxxxx.sandbox0000.opentlc.com]: " wildcard
-wildcard=${wildcard:-Dont forget set a wildcard}
-echo $wildcard
+OCP_DOMAIN=$(oc get IngressController default -n openshift-ingress-operator -o json | jq -r '.status.domain')
 
 ###############################
 ## Create necessary projects ##
@@ -8,23 +6,9 @@ echo $wildcard
 oc new-project central
 oc apply -f deployment/central/minio.yaml
 
-mc alias set minio-central https://minio-api-central.$WILDCARD minio minio123
+mc alias set minio-central https://minio-api-central.$OCP_DOMAIN minio minio123
 mc mb minio-central/workbench
 mc mb minio-central/edge1-data
 mc mb minio-central/edge1-models
 mc mb minio-central/edge1-ready
 
-
-
-
-
-
-
-
-
-
-echo "#####################################"
-echo "######### DON'T FORGET THIS #########"
-echo "#####################################"
-echo "export WILDCARD="$wildcard
-echo "#####################################"
